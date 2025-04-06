03d83dae362e57d0e808663282bcec97a8a1a5cc
Dima Shamis <dims90995@gmail.com>
2025-04-06 21:16:43 +0300

    update

--- server/routes/main.js
+++ server/routes/main.js
@@ -7,7 +7,14 @@ router.get('', (req, res) => {
     res.render('index', {
         title: 'Home',
         layout: './layouts/main',
-    });
+    }); 
+  });
+
+  router.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'about',
+        layout: './layouts/main',
+    }); 
   });
 
 
