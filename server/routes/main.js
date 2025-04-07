2bb732477a9c927df5e9b6b42f91795d3fc2b2ef
Dima Shamis <dims90995@gmail.com>
2025-04-07 19:14:46 +0300

    update

--- server/routes/main.js
+++ server/routes/main.js
@@ -4,17 +4,18 @@ const router = express.Router();
 
 // Routes
 router.get('', (req, res) => {
-    res.render('index', {
-        title: 'Home',
-        layout: './layouts/main',
-    }); 
-  });
+    const locals = {
+        title: 'Blog',
+        description: 'simple Blog',
+    }
+    
+    res.render('index', { locals }); 
+    });
+        
+
 
   router.get('/about', (req, res) => {
-    res.render('about', {
-        title: 'about',
-        layout: './layouts/main',
-    }); 
+    res.render('about' )
   });
 
 
