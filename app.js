9a08af6fcd34ccba224fa8b24a2ea0747328e4a6
Dima Shamis <dims90995@gmail.com>
2025-04-09 20:12:15 +0300

    port

--- app.js
+++ app.js
@@ -150,7 +150,7 @@ app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
